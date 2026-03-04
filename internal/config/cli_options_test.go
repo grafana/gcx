56b5b2acc932007b86c05b5005bf31e69e85d150
Fabrizia Rossano <117294258+frossano-grafana@users.noreply.github.com>
2026-03-04 11:19:36 +0100

    Autoapproval try (#8)

---
+++ internal/config/cli_options_test.go
@@ -0,0 +1,59 @@
+package config_test
+
+import (
+	"testing"
+
+	"github.com/grafana/grafanactl/internal/config"
+)
+
+func TestLoadCLIOptions_AutoApproveTrue(t *testing.T) {
+	t.Setenv("GRAFANACTL_AUTO_APPROVE", "1")
+
+	opts, err := config.LoadCLIOptions()
+	if err != nil {
+		t.Fatalf("LoadCLIOptions() error = %v, want nil", err)
+	}
+
+	if !opts.AutoApprove {
+		t.Errorf("AutoApprove = %v, want true", opts.AutoApprove)
+	}
+}
+
+func TestLoadCLIOptions_AutoApproveTrueString(t *testing.T) {
+	t.Setenv("GRAFANACTL_AUTO_APPROVE", "true")
+
+	opts, err := config.LoadCLIOptions()
+	if err != nil {
+		t.Fatalf("LoadCLIOptions() error = %v, want nil", err)
+	}
+
+	if !opts.AutoApprove {
+		t.Errorf("AutoApprove = %v, want true", opts.AutoApprove)
+	}
+}
+
+func TestLoadCLIOptions_AutoApproveFalse(t *testing.T) {
+	t.Setenv("GRAFANACTL_AUTO_APPROVE", "0")
+
+	opts, err := config.LoadCLIOptions()
+	if err != nil {
+		t.Fatalf("LoadCLIOptions() error = %v, want nil", err)
+	}
+
+	if opts.AutoApprove {
+		t.Errorf("AutoApprove = %v, want false", opts.AutoApprove)
+	}
+}
+
+func TestLoadCLIOptions_AutoApproveEmpty(t *testing.T) {
+	t.Setenv("GRAFANACTL_AUTO_APPROVE", "")
+
+	opts, err := config.LoadCLIOptions()
+	if err != nil {
+		t.Fatalf("LoadCLIOptions() error = %v, want nil", err)
+	}
+
+	if opts.AutoApprove {
+		t.Errorf("AutoApprove = %v, want false (default)", opts.AutoApprove)
+	}
+}
