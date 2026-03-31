40afdbc742b40ba12f48db53312b33dd7a8a8b2c
Patrick Oyarzun <patrick.oyarzun@grafana.com>
2026-03-30 21:15:58 -0400

    test(adaptive-logs): fix testpackage and gci in queryIngestLabel tests

--- internal/providers/adaptive/logs/types_test.go
+++ internal/providers/adaptive/logs/types_test.go
@@ -1,4 +1,4 @@
-package logs
+package logs //nolint:testpackage // Tests unexported queryIngestLabel.
 
 import (
 	"testing"
@@ -11,8 +11,8 @@ func TestQueryIngestLabel(t *testing.T) {
 
 	assert.Equal(t, "N/A", queryIngestLabel(0, 0))
 	assert.Equal(t, "Never", queryIngestLabel(0, 100))
-	assert.Equal(t, "Rarely", queryIngestLabel(1, 1000))   // 0.1%
-	assert.Equal(t, "Rarely", queryIngestLabel(10, 1000))  // 1%
+	assert.Equal(t, "Rarely", queryIngestLabel(1, 1000)) // 0.1%
+	assert.Equal(t, "Rarely", queryIngestLabel(10, 1000)) // 1%
 	assert.Equal(t, "Sometimes", queryIngestLabel(11, 1000))
 	assert.Equal(t, "Sometimes", queryIngestLabel(400, 1000)) // 40%
 	assert.Equal(t, "Often", queryIngestLabel(401, 1000))
